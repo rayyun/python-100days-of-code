067d344ee813d60aa5656797e3fe1b052a64dd60
rayyun <seongeunyun@gmail.com>
2022-05-03 01:27:31 -0700

    update with K-Means

--- Day91-Image-Color-Palette-Generator/main.py
+++ Day91-Image-Color-Palette-Generator/main.py
@@ -1,8 +1,12 @@
+# Day91-Professional Portfolio Project 11 : Image Color Palette Generator
+# K-Means reference link : https://curiousily.com/posts/color-palette-extraction-with-k-means-clustering/
+
 from flask import Flask, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
 import os
-import numpy as np
+from math import sqrt
 from PIL import Image
+import random
 
 dir_path = 'static/uploaded_images'
 
@@ -12,6 +16,117 @@ app.config['SECRET_KEY'] = 'afkjhakfakjsdjka'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 img_path = None
+
+class Point:
+
+    def __init__(self, coordinates):
+        self.coordinates = coordinates
+
+
+class Cluster:
+
+    def __init__(self, center, points):
+        self.center = center
+        self.points = points
+
+
+class KMeans:
+
+    def __init__(self, n_clusters, min_diff=1):
+        self.n_clusters = n_clusters
+        self.min_diff = min_diff
+
+    def calculate_center(self, points):
+        n_dim = len(points[0].coordinates)
+        vals = [0.0 for i in range(n_dim)]
+        for p in points:
+            for i in range(n_dim):
+                vals[i] += p.coordinates[i]
+
+        coords = [(v / len(points)) for v in vals]
+
+        return Point(coords)
+
+    def assign_points(self, clusters, points):
+        plists = [[] for i in range(self.n_clusters)]
+
+        for p in points:
+            smallest_distance = float('inf')
+
+            for i in range(self.n_clusters):
+                distance = euclidean(p, clusters[i].center)
+                if distance < smallest_distance:
+                    smallest_distance = distance
+                    idx = i
+
+            plists[idx].append(p)
+
+        return plists
+
+    def fit(self, points):
+        clusters = [Cluster(center=p, points=[p]) for p in random.sample(points, self.n_clusters)]
+
+        while True:
+            plists = self.assign_points(clusters, points)
+
+            diff = 0
+
+            for i in range(self.n_clusters):
+                if not plists[i]:
+                    continue
+
+                old = clusters[i]
+                center = self.calculate_center(plists[i])
+                new = Cluster(center, plists[i])
+                clusters[i] = new
+                diff = max(diff, euclidean(old.center, new.center))
+
+            if diff < self.min_diff:
+                break
+
+        return clusters
+
+
+def euclidean(p, q):
+    n_dim = len(p.coordinates)
+    return sqrt(sum([(p.coordinates[i] - q.coordinates[i]) ** 2 for i in range(n_dim)]))
+
+
+def get_points(image_path):
+    img = Image.open(image_path)
+    img.thumbnail((200, 400))
+    img = img.convert('RGB')
+    w, h = img.size
+
+    points = []
+    for count, color in img.getcolors(w * h):
+        for _ in range(count):
+            points.append(Point(color))
+
+    return points
+
+def rgb_to_hex(rgb):
+    # return '#%s' % ''.join(('%02x' % p for p in rgb))
+    return ''.join(('%02x' % p for p in rgb))
+
+
+def hex_to_rgb(hex):
+    rgb = []
+    for i in (0, 2, 4):
+        decimal = int(hex[i:i + 2], 16)
+        rgb.append(decimal)
+
+    return tuple(rgb)
+
+
+def get_colors(filename, n_colors=10):
+    points = get_points(filename)
+    clusters = KMeans(n_clusters=n_colors).fit(points)
+    clusters.sort(key=lambda c: len(c.points), reverse=True)
+    rgbs = [map(int, c.center.coordinates) for c in clusters]
+
+    return list(map(hex_to_rgb, (map(rgb_to_hex, rgbs))))
+
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
@@ -27,33 +142,14 @@ def home():
         return redirect(url_for('home'))
 
     elif request.method == 'GET' and img_path:
-        img = np.array(Image.open(img_path).convert('RGB'))
+        top_ten_colors = get_colors(img_path, n_colors=10)
 
-        # For png, delete opacity column
-        if img.shape[2] == 4:
-            img = np.delete(img, 3, 2)
+        print("top_ten_colors", top_ten_colors)
 
-        img = img.reshape(-1, 3)
-
-        # Counting all the unique colors
-        (unique, count) = np.unique(img, axis=0, return_counts=True)
-
-        # turn two separate variables into tuple (color code, its corresponding count)
-        color_tuple = list(zip(unique, count))
-
-        # sort the colors in descending order of count
-        color_tuple = sorted(color_tuple, key=lambda x: x[1], reverse=True)
-
-        # extract the top 10 colors, turn the color code from ndarray to python list of int.
-        top_ten_colors = [color[0].astype(int).tolist() for color in color_tuple[:10]]
-        print(top_ten_colors)
         return render_template('index.html', colors=top_ten_colors, imgpath=img_path)
 
     return render_template('index.html')
 
-
-def rgb_to_hex(rgb):
-    return '%02x%02x%02x' % tuple(rgb)
 
 if __name__ == "__main__":
     app.run(debug=True)
